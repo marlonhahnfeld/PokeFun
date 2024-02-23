a54c45c41f109261e6c2f2e0c53660525fac97e8
acq570 <marlon.misiak-hahnfeld@haw-hamburg.de>
2024-02-23 19:49:38 +0100

    ready for v1 higher lower

--- src/pages/HigherLowerGamePage.js
+++ src/pages/HigherLowerGamePage.js
@@ -1,65 +1,33 @@
 import React from "react";
-//import "./HigherLowerGamePage.css";
 import "./HigherLowerPageCSS.css";
-import { useState, useEffect } from "react";
-import { randomPokemonId, sumBaseStats } from "../utils/HigherLowerUtil";
+import { useState } from "react";
+import usePokemonFetch from "../hooks/usePokemonFetch";
+import { sumBaseStats } from "../utils/HigherLowerUtil";
 import PokemonCard from "../components/PokemonCard";
 
 const HigherLowerGamePage = () => {
-  const [pokemon1, setPokemon1] = useState("");
-  const [pokemon2, setPokemon2] = useState("");
   const [score, setScore] = useState(0);
-  const [pokemon1TotalStats, setPokemon1TotalStats] = useState("?");
-  const [pokemon2TotalStats, setPokemon2TotalStats] = useState("?");
   const [roundDone, setRoundDone] = useState(false);
   const [isClickable, setIsClickable] = useState(true);
+  const { pokemons, totalStats, setTotalStats } = usePokemonFetch(roundDone, 2);
 
-  // FETCHING von 2 Pokemon
-  useEffect(() => {
-    const fetchPokemon = async () => {
-      const randomNumber1 = randomPokemonId();
-      const randomNumber2 = randomPokemonId();
-
-      const url1 = `https://pokeapi.co/api/v2/pokemon/${randomNumber1}/`;
-      const url2 = `https://pokeapi.co/api/v2/pokemon/${randomNumber2}/`;
-
-      try {
-        const response1 = await fetch(url1);
-        const data1 = await response1.json();
-        setPokemon1(data1);
-
-        const response2 = await fetch(url2);
-        const data2 = await response2.json();
-        setPokemon2(data2);
-
-        console.log(data1.name);
-        console.log(data2.name);
-      } catch (error) {
-        console.error("Error fetching Pokemon:", error);
-      }
-    };
-
-    fetchPokemon();
-  }, [roundDone]);
-
-  const handleClickCard1 = async () => {
+  const handleClickCard = (cardNumber) => {
     // Check if the card is clickable
     if (!isClickable) {
       return;
     }
-
     // Set the card to not be clickable
     setIsClickable(false);
-
-    const totalStatsForPokemon1 = sumBaseStats(pokemon1);
-    const totalStatsForPokemon2 = sumBaseStats(pokemon2);
-
+    const totalStatsForPokemon1 = sumBaseStats(pokemons[0]);
+    const totalStatsForPokemon2 = sumBaseStats(pokemons[1]);
     // Show the total stats
-    setPokemon1TotalStats(totalStatsForPokemon1);
-    setPokemon2TotalStats(totalStatsForPokemon2);
+    setTotalStats([totalStatsForPokemon1, totalStatsForPokemon2]);
 
     // Compare the stats and increase the score
-    if (totalStatsForPokemon1 >= totalStatsForPokemon2) {
+    if (
+      (cardNumber === 1 && totalStatsForPokemon1 >= totalStatsForPokemon2) ||
+      (cardNumber === 2 && totalStatsForPokemon1 <= totalStatsForPokemon2)
+    ) {
       setScore(score + 1);
     } else {
       setScore(0);
@@ -67,77 +35,36 @@ const HigherLowerGamePage = () => {
 
     const timeoutId = setTimeout(() => {
       setRoundDone((prev) => !prev);
-
-      setPokemon1TotalStats("?");
-      setPokemon2TotalStats("?");
-
       // Clear timeout if component unmounts or new round starts
       clearTimeout(timeoutId);
 
       // Set the card to be clickable again after 1 second
       setIsClickable(true);
-    }, 1000);
-  };
-
-  const handleClickCard2 = async () => {
-    // Check if the card is clickable
-    if (!isClickable) {
-      return;
-    }
-
-    // Set the card to not be clickable
-    setIsClickable(false);
-
-    const totalStatsForPokemon1 = sumBaseStats(pokemon1);
-    const totalStatsForPokemon2 = sumBaseStats(pokemon2);
-
-    // Show the total stats
-    setPokemon1TotalStats(totalStatsForPokemon1);
-    setPokemon2TotalStats(totalStatsForPokemon2);
-
-    // Compare the stats and increase the score
-    if (totalStatsForPokemon1 <= totalStatsForPokemon2) {
-      setScore(score + 1);
-    } else {
-      setScore(0);
-    }
-
-    const timeoutId = setTimeout(() => {
-      setRoundDone((prev) => !prev);
-
-      setPokemon1TotalStats("?");
-      setPokemon2TotalStats("?");
-
-      // Clear timeout if component unmounts or new round starts
-      clearTimeout(timeoutId);
-
-      // Set the card to be clickable again after 1 second
-      setIsClickable(true);
-    }, 1000);
+    }, 500);
   };
 
   return (
     <div className="title">
-      <h4 className="score">Score:{score} </h4>
+      <h4 className="score">Score: {score}</h4>
       <div className="app">
         <div className="container">
           <PokemonCard
-            pokemon={pokemon1}
+            pokemon={pokemons[0]}
             id="1"
-            totalStats={pokemon1TotalStats}
+            totalStats={totalStats[0]}
             onClick={() => {
-              handleClickCard1();
+              handleClickCard(1);
             }}
           />
         </div>
         <div className="placeholder"></div>
         <div className="container">
           <PokemonCard
-            pokemon={pokemon2}
+            pokemon={pokemons[1]}
             id="2"
-            totalStats={pokemon2TotalStats}
+            totalStats={totalStats[1]}
             onClick={() => {
-              handleClickCard2();
+              handleClickCard(2);
             }}
           />
         </div>
