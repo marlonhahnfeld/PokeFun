2728fbc021a12e621fb3aa95c14ed188b5311b39
acq570 <marlon.misiak-hahnfeld@haw-hamburg.de>
2024-03-03 21:03:40 +0100

    imported framer motion
    first motion animation for lpcard

--- src/components/LPCard.jsx
+++ src/components/LPCard.jsx
@@ -5,36 +5,45 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { Button, CardActionArea, CardActions } from "@mui/material";
 import "../styles/LPCard.css";
+import { motion } from "framer-motion";
 
 const LPCard = ({ title, description, image }) => {
   return (
-    <Card sx={{ maxWidth: 345 }} className="lpcard">
-      <CardActionArea className="lpcardactionarea">
-        <CardMedia
-          component="img"
-          height="200"
-          image={image}
-          alt="game mode card"
-        />
-        <CardContent className="lpcardcontent">
-          <Typography gutterBottom variant="h5" component="div">
-            {title}
-          </Typography>
-          <Typography
-            variant="body2"
-            color="text.secondary"
-            className="lpcarddescription"
-          >
-            {description}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions className="lpcardbottom">
-        <Button size="small" color="primary">
-          Placeholder
-        </Button>
-      </CardActions>
-    </Card>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1, ease: "easeInOut" }}
+      whileHover={{ scale: 1.2 }}
+      style={{ maxWidth: 345 }}
+    >
+      <Card sx={{ maxWidth: 345 }} className="lpcard">
+        <CardActionArea className="lpcardactionarea">
+          <CardMedia
+            component="img"
+            height="200"
+            image={image}
+            alt="game mode card"
+          />
+          <CardContent className="lpcardcontent">
+            <Typography gutterBottom variant="h5" component="div">
+              {title}
+            </Typography>
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              className="lpcarddescription"
+            >
+              {description}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions className="lpcardbottom">
+          <Button size="small" color="primary">
+            Placeholder
+          </Button>
+        </CardActions>
+      </Card>
+    </motion.div>
   );
 };
 
