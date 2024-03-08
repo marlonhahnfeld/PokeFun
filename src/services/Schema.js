098c965e1d76bfe65077a500370ff1b77e3baa75
acq570 <marlon.misiak-hahnfeld@haw-hamburg.de>
2024-03-08 17:57:47 +0100

    deleted unwanted files

--- src/services/Schema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-  name: String,
-  sprite: String,
-});
-
-//model gibt an, wie die collection heißt und welches schema verwendet wird
-module.exports = mongoose.model("GuessThePokemon", userSchema);
