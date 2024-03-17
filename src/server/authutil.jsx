cc0ce88c82e4e261daa2a63df99bcdbc84b9d84c
KevinZawieja <kevin_zawieja@hotmail.de>
2024-03-17 16:46:48 +0100

    - registerUser

--- src/server/authutil.jsx
+++ src/server/authutil.jsx
@@ -1,33 +1,33 @@
 export const registerUser = async (username, password) => {
-  return fetch("http://poke-fun-blush.vercel.app/registerUser", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    credentials: "include", // Include the cookies
+  const url = "https://poke-fun-blush.vercel.app/registerUser";
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include", // Include the cookies
 
-    body: JSON.stringify({
-      username: username,
-      password: password,
-    }),
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      return data; // return the data so it can be used by the function that calls registerUserAndPasswordToMongo
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-      throw error; // re-throw the error so it can be caught by the function that calls registerUserAndPasswordToMongo
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
     });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data; // return the data so it can be used by the function that calls registerUserAndPasswordToMongo
+  } catch (error) {
+    console.error(`Error fetching ${url}:`, error);
+    throw error; // re-throw the error so it can be caught by the function that calls registerUserAndPasswordToMongo
+  }
 };
 
 export const loginUser = async (username, password) => {
-  return fetch("http://poke-fun-blush.vercel.app/loginUser", {
+  return fetch("https://poke-fun-blush.vercel.app/loginUser", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
