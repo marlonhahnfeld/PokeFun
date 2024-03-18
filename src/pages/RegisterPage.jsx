887f31897b0c76def371c482cdd62ae35a3d3878
Marlon Hahnfeld <130953452+marlonhahnfeld@users.noreply.github.com>
2024-03-18 19:05:03 +0100

    added logo link to landing page in loginpage and registerpage

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import RegisterForm from "../components/RegisterForm";
 import "../styles/RegisterPage.css";
+import Logo from "../components/Logo";
+import { Link } from "react-router-dom";
 
 const RegisterPage = () => {
   return (
     <div className="register-page">
+      <Link to={"/"}>
+        <Logo text={"PokeFun"} />
+      </Link>
       <div className="register-form-border">
         <RegisterForm />
       </div>
