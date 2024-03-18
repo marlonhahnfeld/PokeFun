92bf6508c40f3f5fba32bf24e6be893adfbb5513
Marlon Hahnfeld <130953452+marlonhahnfeld@users.noreply.github.com>
2024-03-18 16:28:15 +0100

    background lp und hl

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -58,6 +58,8 @@ const LoginForm = () => {
           variant="outlined"
           size="medium"
           margin="normal"
+          color="primary"
+          InputLabelProps={{ style: { color: "white" } }}
         />
         <TextField
           {...register("password", { required: "Password is required" })}
@@ -66,6 +68,7 @@ const LoginForm = () => {
           variant="outlined"
           size="medium"
           margin="normal"
+          InputLabelProps={{ style: { color: "white" } }}
         />
         <Button
           disabled={isSubmitting}
